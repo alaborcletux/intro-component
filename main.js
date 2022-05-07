ab3d54ecf2444580a132451f19fb9e57dd738e9d
Stephanie Ezinne <stephanieegbuonu@gmail.com>
2022-05-07 22:45:56 +0100

    intro component

---
+++ main.js
@@ -0,0 +1,19 @@
+function validate() {
+    var firstName = document.getElementById("firstname").ariaValueMax;
+    var lastName = document.getElementById("lastname").ariaValueMax;
+    var formEmail = document.getElementById("email").ariaValueMax;
+    var formPassword = document.getElementById("password").ariaValueMax;
+    if (firstName == null || firstName == "",lastName == null || lastName == "",formEmail == null || formEmail == "",formPassword == null || formPassword == ""){
+        
+        document.getElementById("fnlabel").style.visibility="visible";
+        document.getElementById("fnimg").style.visibility="visible";
+        document.getElementById("lnlabel").style.visibility="visible";
+        document.getElementById("lnimg").style.visibility="visible";
+        document.getElementById("emaillabel").style.visibility="visible";
+        document.getElementById("emailimg").style.visibility="visible";
+        document.getElementById("passlabel").style.visibility="visible";
+        document.getElementById("passimg").style.visibility="visible";
+
+        return false;
+    }
+}
